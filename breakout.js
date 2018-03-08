f3f4397a2fb1afce8932db99bb380b59317909b5
tommyling2 <35809324+tommyling2@users.noreply.github.com>
2018-03-08 16:28:54 +0000

    Add files via upload
    Moving the paddle

--- breakout.js
+++ breakout.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var ballRadius = 10;
 var x = canvas.width/2;
-var y = canvas.height-20;
+var y = canvas.height-30;
 var dx = -2;
 var dy = -2;
 
@@ -10,7 +10,8 @@ var dy = -2;
 var paddleHeight = 10;
 var paddleWidth = 75;
 var paddleX = (canvas.width - paddleWidth)/2;
-
+var rightPressed = false;
+var leftPressed = false;
 
 function drawBall()
 {
@@ -34,12 +35,20 @@ function draw() {
 	ctx.clearRect(0,0,canvas.width,canvas.height);
 	//Draw the ball
 	drawBall();
-	
+	//Draw the paddle
 	drawPaddle();
+
 
 	
 	x += dx;
 	y += dy;
+	
+	if(rightPressed) {
+		paddleX += 7;
+	}
+	else if(leftPressed) {
+		paddleX -= 7;
+	}
 	
 	//Bounce the ball off
 	if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
@@ -48,8 +57,28 @@ function draw() {
 	if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
 		dy = -dy;
 	}
-
 }	
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+	if(e.keyCode == 39) {
+		rightPressed = true;
+	}
+else if(e.keyCode == 37) {
+	leftPressed = true;
+}
+	}
+
+function keyUpHandler(e) {
+	if(e.keyCode == 39) {
+		rightPressed = false;
+	}
+	else if(e.keyCode == 37) {
+		leftPressed = false;
+	}
+}
 
 setInterval(draw,10);
 
