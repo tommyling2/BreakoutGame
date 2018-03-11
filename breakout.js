7e429ed626525aaa7146f5842bbb295f939f6438
tommyling2 <35809324+tommyling2@users.noreply.github.com>
2018-03-11 18:28:07 +0000

    Add files via upload
    Draw the bricks

--- breakout.js
+++ breakout.js
@@ -13,6 +13,42 @@ var paddleX = (canvas.width - paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
 
+//Setup some bricks
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+//Hold the bricks in a two-dimensional array = think of it as rows and columns 
+var bricks = [];
+for (c=0; c < brickColumnCount; c++) {
+	bricks[c] = [];
+	for (r=0; r < brickRowCount; r++) {
+		bricks [c][r] = { x:0, y:0, status: 1 } ;
+	}
+}
+
+//This function draws the bricks
+function drrawBricks() {
+	for(c=0; c < brickColumnCount; c++) {
+		for(r=0; r < brickRowCount; r++) {
+			var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+			var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+			bricks[c][r].x = brickX;
+			bricks[c][r].y = brickY;
+			ctx.beginPath();
+			ctx.rect(brickX, brickY, brickWidth, brickHeight);
+			ctx.fillStyle = "blue";
+			ctx.fill();
+			ctx.closePath();
+		}
+	}
+}
+
+			
 function drawBall()
 {
 ctx.beginPath();
@@ -37,6 +73,8 @@ function draw() {
 	drawBall();
 	//Draw the paddle
 	drawPaddle();
+	//Draw the bricks
+	drrawBricks();
 
 
 	
